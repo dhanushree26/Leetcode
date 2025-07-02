be41999424300b533f1c078cb253f4d6a50f23ff
dhanushree26 <136553699+dhanushree26@users.noreply.github.com>
2025-07-02 11:42:51 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (33.84%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,29 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
         s=s.trim();
-        long n = 0;
-        int sign = 1;
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
-                n=n*10+Integer.valueOf(s.charAt(i)-'0');
-                if((n*sign)>Integer.MAX_VALUE){
-                    return Integer.MAX_VALUE;
-                }
-                if((n*sign)<Integer.MIN_VALUE){
-                    return Integer.MIN_VALUE;
-                }
-            }
-            else if(i==0 && s.charAt(i)=='-'){
-                sign = -1;
-            }
-            else if(i==0 && s.charAt(i)=='+'){
-                sign = 1;
-            }
-            else{
-                break;
-            }
+        if(s.isEmpty()){
+            return 0;
         }
-        
-        return (int)n*sign;
+        int sign = 1;
+        int idx = 0;
+        if(s.charAt(idx)=='-'){
+            sign = -1;
+            idx++;
+        }
+        else if(s.charAt(idx)=='+'){
+            idx++;
+        }
+        return helper(s,idx,0,sign);
+    }
+    public int helper(String s,int idx,int res,int sign){
+        if(idx>=s.length() ||!Character.isDigit(s.charAt(idx))){
+            return sign*res;
+        }
+        int dig = s.charAt(idx)-'0';
+        if(sign==1 && res > (Integer.MAX_VALUE-dig)/10){
+            return Integer.MAX_VALUE;
+        }
+        if(sign==-1 && res > (Integer.MAX_VALUE-dig)/10){
+            return Integer.MIN_VALUE;
+        }
+        return helper(s,idx+1,res*10+dig,sign);
     }
 }
