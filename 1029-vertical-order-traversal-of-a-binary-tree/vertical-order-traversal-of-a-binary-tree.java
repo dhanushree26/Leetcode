38d43e02bad1eaea82daf912cca1a8013414bd9b
dhanushree26 <136553699+dhanushree26@users.noreply.github.com>
2025-07-12 22:44:16 +0530

    Time: 2 ms (99.56%) | Memory: 42.9 MB (13.84%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -13,36 +13,10 @@
  *     }
  * }
  */
- class Tuple{
-    TreeNode root;
-    int row;
-    int col;
-    public Tuple(TreeNode _root,int _row,int _col){
-        this.root = _root;
-        this.row = _row;
-        this.col = _col;
-    }
- }
 class Solution {
+    TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Tuple> q = new LinkedList<>();
-        q.offer(new Tuple(root,0,0));
-        while(!q.isEmpty()){
-            Tuple tuple = q.poll();
-            TreeNode node = tuple.root;
-            int row = tuple.row;
-            int col = tuple.col;
-            map.putIfAbsent(col,new TreeMap<>());
-            map.get(col).putIfAbsent(row,new PriorityQueue<>());
-            map.get(col).get(row).offer(node.val);
-            if(node.left!=null){
-                q.offer(new Tuple(node.left,row+1,col-1));
-            }
-            if(node.right!=null){
-                q.offer(new Tuple(node.right,row+1,col+1));
-            }
-        }
+        dfs(root,0,0);
         List<List<Integer>> res = new ArrayList<>();
         for(TreeMap<Integer,PriorityQueue<Integer>> map2 : map.values()){
             List<Integer> list = new ArrayList<>();
@@ -54,5 +28,16 @@ class Solution {
             res.add(list);
         }
         return res;
+    }
+    public void dfs(TreeNode root,int row,int col){
+        if(root == null){
+            return;
+        }
+        map.putIfAbsent(col,new TreeMap<>());
+        map.get(col).putIfAbsent(row,new PriorityQueue<>());
+        map.get(col).get(row).offer(root.val);
+
+        dfs(root.left,row+1,col-1);
+        dfs(root.right,row+1,col+1);
     }
 }
