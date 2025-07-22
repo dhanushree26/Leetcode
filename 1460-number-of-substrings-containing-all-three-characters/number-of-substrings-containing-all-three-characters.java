be9b811fd8806bcc9947bf7a687e228ab202321f
dhanushree26 <136553699+dhanushree26@users.noreply.github.com>
2025-07-22 21:11:24 +0530

    Time: 7 ms (98.46%) | Memory: 44.5 MB (79.85%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -8,12 +8,11 @@ class Solution {
         while(r<n){
             hash[s.charAt(r)-'a'] = r;
             int min = n;
-            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
                 for(int i=0;i<3;i++){
                     min = Math.min(min,hash[i]);
                 }
                 cnt+=(min+1);
-            }
+            
             r++;
         }
         return cnt;
