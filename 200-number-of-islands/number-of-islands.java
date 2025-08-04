4f20ae8a4513602c41be9672b6623592328184b3
dhanushree26 <136553699+dhanushree26@users.noreply.github.com>
2025-08-04 22:18:33 +0530

    Time: 6 ms (23.10%) | Memory: 52.3 MB (16.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,3 +1,11 @@
+class Pair{
+    int r;
+    int c;
+    Pair(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
+}
 class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
@@ -8,25 +16,31 @@ class Solution {
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1' && !vis[i][j]){
                     cnt++;
-                    dfs(grid,vis,i,j);   
+                    bfs(grid,vis,i,j);   
                 }
             }
         }
         return cnt;
     }
-    public void dfs(char[][] grid, boolean[][] vis,int i,int j){
+    public void bfs(char[][] grid, boolean[][] vis,int i,int j){
         int n = grid.length;
         int m = grid[0].length;
-        if(i<0 || i>=n || j<0 || j>=m){
-            return;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(!q.isEmpty()){
+            int row = q.peek().r;
+            int col = q.peek().c;
+            q.remove();
+            int[] drow = {-1,0,1,0};
+            int[] dcol = {0,1,0,-1};
+            for(int k=0;k<4;k++){
+                int nrow = drow[k]+row;
+                int ncol = dcol[k]+col;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                    q.add(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=true;
+                }
+            }
         }
-        if(grid[i][j]=='0' || vis[i][j]){
-            return;
-        }
-        vis[i][j] = true;
-        dfs(grid,vis,i+1,j);
-        dfs(grid,vis,i-1,j);
-        dfs(grid,vis,i,j+1);
-        dfs(grid,vis,i,j-1);
     }
 }
