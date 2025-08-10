fe999e1d373a9aa81ca6faa6d806fe84ac6eb0da
dhanushree26 <136553699+dhanushree26@users.noreply.github.com>
2025-08-10 20:53:46 +0530

    Time: 1 ms (99.22%) | Memory: 43.7 MB (18.89%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -1,30 +1,28 @@
 class Solution {
     public char kthCharacter(long k, int[] operations) {
         int n = operations.length;
-        // Compute lengths with capping
-        long[] lengths = new long[n + 1];
-        lengths[0] = 1;
+        long[] len = new long[n + 1];
+        len[0] = 1;
         for (int i = 1; i <= n; i++) {
-            if (lengths[i - 1] <= Long.MAX_VALUE / 2) {
-                lengths[i] = lengths[i - 1] * 2;
+            if (len[i - 1] <= Long.MAX_VALUE / 2) {
+                len[i] = len[i - 1] * 2;
             } else {
-                lengths[i] = Long.MAX_VALUE; // cap to avoid overflow
+                len[i] = Long.MAX_VALUE;
             }
         }
-        return helper(operations, n - 1, k, 0L, lengths);
+        return helper(operations, n - 1, k, 0, len);
     }
-    
-    public char helper(int[] ops, int i, long k, long cnt, long[] lengths) {
-    if (i < 0) {
-        return (char) ('a' + (cnt % 26));
-    }
-    long half = lengths[i + 1] / 2;
-    if (k > half) {
-        k -= half;
-        if (ops[i] == 1) {
-            cnt++;
+    public char helper(int[] ops, int i, long k, long cnt, long[] len) {
+        if (i < 0) {
+            return (char) ('a' + (cnt % 26));
         }
-    }
-    return helper(ops, i - 1, k, cnt, lengths);
+        long half = len[i + 1] / 2;
+        if (k > half) {
+            k = k - half;
+            if (ops[i] == 1) {
+                cnt++;
+            }
+        }
+        return helper(ops, i - 1, k, cnt, len);
     }
 }
